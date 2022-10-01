e851d72886936e3ae66fd32ce6ec8733cdc489d0
José Lucas <bezerra00714@hotmail.com>
2022-09-30 21:44:21 -0300

    adicionado algunas arquivos

---
+++ Java Web/src/main/java/connectionFactory/ConnectionFactory.java
@@ -0,0 +1,30 @@
+package connectionFactory;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+	private static final String DATABASE_URL = "jdbc:mysql://localhost:3306/";
+	private static final String USERNAME = "root";
+	private static final String PASSWORD = "";
+	private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+	
+	
+	public static Connection createConnection() {
+		try {
+			Class.forName(DRIVER);
+			System.out.println("Driver found.");
+		}catch(ClassNotFoundException e) {
+			System.out.println("Driver not found. " + e.getMessage());
+		}
+		try {
+			Connection connection = DriverManager.getConnection(DATABASE_URL, USERNAME, PASSWORD);
+			System.out.println("Connected to database.");
+			return connection;
+		}catch(SQLException e) {
+			System.out.println("Not connected to database. "+ e.getMessage());
+			return null;
+		}
+	}
+}
