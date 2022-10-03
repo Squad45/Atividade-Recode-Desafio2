0e156f47a6232b92e56da0d2c403ede378510888
José Lucas <bezerra00714@hotmail.com>
2022-10-03 11:23:38 -0300

    adicionado animação de scroll e servlet começado

--- Java Web/src/main/java/controllers/CreateCadastro.java
+++ Java Web/src/main/java/controllers/CreateCadastro.java
@@ -1,11 +1,15 @@
 package controllers;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import model.Cadastro;
 
 /**
  * Servlet implementation class CreateCadastro
@@ -13,6 +17,7 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet({ "/criandoCadastro", "/pessoaFisica", "/pessoaJuridica" })
 public class CreateCadastro extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	Cadastro cliente = new Cadastro();
        
     /**
      * @see HttpServlet#HttpServlet()
@@ -26,15 +31,29 @@ public class CreateCadastro extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		String action= request.getServletPath();
+		
+		if(action.equals("/pessoaJuridica")) {
+			System.out.println("enviando para cadastro juridico");
+			RequestDispatcher requestDispatcher = request.getRequestDispatcher("cadastro2_empresa.html");
+			requestDispatcher.forward(request, response);
+		}else if(action.equals("/pessoaFisica")) {
+			System.out.println("enviando para cadastro fisico");
+			RequestDispatcher requestDispatcher = request.getRequestDispatcher("cadastro3_pessoa.html");
+			requestDispatcher.forward(request, response);
+		}
+		
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		String action= request.getServletPath();
+		if(action.equals("/pessoaJuridica")) {
+			cliente.setEscolha("Juridica");
+			System.out.println(cliente.getEscolha());
+		}else if(action.equals("/pessoaFisica")) {
+			cliente.setEscolha("Fisica");
+			System.out.println(cliente.getEscolha());
+		}
 		doGet(request, response);
 	}
 
